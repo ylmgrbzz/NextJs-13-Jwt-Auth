ee9625d5ec1d775bec29cf5b04c13ece5e72794f
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-03-30 22:05:34 +0300

    Update jose dependency to version 4.15.5

---
+++ src/libs/auth/index.js
@@ -0,0 +1,18 @@
+import { jwtVerify } from "jose";
+
+const getJwtSecretKey = () => {
+  const secretKey = process.env.JWT_SECRET_KEY;
+  if (!secretKey) {
+    throw new Error("JWT_SECRET_KEY is not defined");
+  }
+  return new TextEncoder().encode(secretKey);
+};
+
+export async function verifyJwtToken(token) {
+  try {
+    const { payload } = await jwtVerify(token, getJwtSecretKey());
+    return payload;
+  } catch (error) {
+    return null;
+  }
+}
